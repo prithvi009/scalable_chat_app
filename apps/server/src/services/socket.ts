7274507dd3cf6644d104ac9bac202233ee327327
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-12-19 21:41:13 +0530

    feat: socket connection backend

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,25 @@
+import {Server} from 'socket.io'
+
+class SocketService{
+    private _io: Server;
+    constructor(){
+        console.log("init socket server")
+        this._io = new Server();
+    }
+
+    public initListeners(){
+        const io = this.io;
+        io.on('connect', (socket)=>{
+            console.log('new socket connected ', socket.id);
+            socket.on("event:message", async({message} : {message:string} )=>{
+                console.log(`new message recieved ${message}`)
+            })
+        })
+    }
+
+    get io(){
+        return this._io;
+    }
+}
+
+export default SocketService;
---
+++ apps/web/context/socketProvider.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React,{useCallback} from "react"
+
+interface SocketProviderProps{
+    children?: React.ReactNode
+}
+
+interface ISocketContext{
+    sendMessage:(msg: string) => any;
+}
+
+const SocketContext = React.createContext<ISocketContext | null >(null);
+
+export const SocketProvider : React.FC<SocketProviderProps> = ({children})=>{
+
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg)=>{
+        console.log("send message")
+    },[])
+    return(
+        <SocketContext.Provider value={null}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
