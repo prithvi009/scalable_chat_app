7865d249f7f6e6e73e0a4746b504a52a99c9c035
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-01-03 22:06:55 +0530

    feat: socket provider

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ class SocketService{
     private _io: Server;
     constructor(){
         console.log("init socket server")
-        this._io = new Server();
+        this._io = new Server({
+            cors:{
+                allowedHeaders: ["*"],
+                origin: "*",
+            }
+        });
     }
 
     public initListeners(){
--- apps/web/context/socketProvider.tsx
+++ apps/web/context/socketProvider.tsx
@@ -1,24 +1,61 @@
-'use client'
-import React,{useCallback} from "react"
+"use client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
-interface SocketProviderProps{
-    children?: React.ReactNode
+interface SocketProviderProps {
+  children?: React.ReactNode;
 }
 
-interface ISocketContext{
-    sendMessage:(msg: string) => any;
+interface ISocketContext {
+  sendMessage: (msg: string) => any;
+  messages: string[];
 }
 
-const SocketContext = React.createContext<ISocketContext | null >(null);
+const SocketContext = React.createContext<ISocketContext | null>(null);
 
-export const SocketProvider : React.FC<SocketProviderProps> = ({children})=>{
+export const useSocket = () => {
+  const state = useContext(SocketContext);
+  if (!state) throw new Error(`state is undefined`);
 
-    const sendMessage: ISocketContext['sendMessage'] = useCallback((msg)=>{
-        console.log("send message")
-    },[])
-    return(
-        <SocketContext.Provider value={null}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+  return state;
+};
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket>();
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (msg) => {
+      console.log("Send Message", msg);
+      if (socket) {
+        socket.emit("event:message", { message: msg });
+      }
+    },
+    [socket]
+  );
+
+  const onMessageRec = useCallback((msg: string) => {
+    console.log("From Server Msg Rec", msg);
+    const { message } = JSON.parse(msg) as { message: string };
+    setMessages((prev) => [...prev, message]);
+  }, []);
+
+  useEffect(() => {
+    const _socket = io("http://localhost:8000");
+    _socket.on("message", onMessageRec);
+
+    setSocket(_socket);
+
+    return () => {
+      _socket.off("message", onMessageRec);
+      _socket.disconnect();
+      setSocket(undefined);
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ sendMessage, messages }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
