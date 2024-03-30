d6e96f84207ca7e1f173991422a2320041a3f6cc
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2024-03-30 22:16:27 +0530

    feat: typescript setup

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
